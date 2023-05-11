0cb1a1b0d3e0142b307843a66a83ec2b371cca3d
myw219 <myw219@lehigh.edu>
2023-05-10 22:36:11 -0400

    added constraints on attacks

--- src/agent/intention/explorerIntentions/attackIntention.py
+++ src/agent/intention/explorerIntentions/attackIntention.py
@@ -8,6 +8,8 @@ from agent.intention.commonIntentions import TravelIntention, DetachBlocksIntent
 
 from agent.action import AgentAction, ClearAction, SkipAction
 
+import random
+
 class AttackIntention(AgentIntention):
     """
     Intention to search out an enemy agent to disable them.
@@ -34,52 +36,51 @@ class AttackIntention(AgentIntention):
         return 6.0
 
     async def planNextAction(self, observation: Observation) -> AgentAction:
-
         enemyCoord = self.getClearableEnemy(observation)
         if(enemyCoord is not None):
-            self.finsihed = True
+            self.finished = True
             return ClearAction(Coordinate.getRelativeCoordinate(observation.agentCurrentCoordinate,enemyCoord))
         else:
-            return await ExploreIntention.planNextAction(self, observation)
+            # return await ExploreIntention.planNextAction(self, observation)
             # if the given area is unknown then travel there to get info about that area
-            # if observation.map.getMapValueEnum(self.targetCoordinate) == MapValueEnum.UNKNOWN:
-            #     if self.travelIntention is None:
-            #         self.travelIntention = TravelIntention(self.targetCoordinate)
+            if observation.map.getMapValueEnum(self.targetCoordinate) == MapValueEnum.UNKNOWN:
+                if self.travelIntention is None:
+                    self.travelIntention = TravelIntention(self.targetCoordinate)
             
-                # return await self.travelIntention.planNextAction(observation)
-                 # If carrying a block and it regulates a norm then drop it
-            # maxBlockCount = observation.simDataServer.getMaxBlockRegulation()
-            # if len(observation.agentData.attachedEntities) > 1 or (maxBlockCount is not None and maxBlockCount < len(observation.agentData.attachedEntities)):
-            #     if self.detachBlocksIntention is None:
-            #         self.detachBlocksIntention = DetachBlocksIntention()
+                return await self.travelIntention.planNextAction(observation)
+                #  If carrying a block and it regulates a norm then drop it
+            maxBlockCount = observation.simDataServer.getMaxBlockRegulation()
+            if len(observation.agentData.attachedEntities) > 1 or (maxBlockCount is not None and maxBlockCount < len(observation.agentData.attachedEntities)):
+                if self.detachBlocksIntention is None:
+                    self.detachBlocksIntention = DetachBlocksIntention()
                 
-            #     return await self.detachBlocksIntention.planNextAction(observation)
+                return await self.detachBlocksIntention.planNextAction(observation)
 
-            # # If there are reserved roles for the Agent and it is not already adopted
-            # # then adopt it. It is important because of the Norms
-            # agentRoles = observation.simDataServer.getReservedRolesForAgent(observation.agentData.id)
-            # if any(agentRoles) and observation.agentMapcRole != agentRoles[0]:
-            #     return await self.planRoleAdoptPlan(observation, agentRoles[0])
+            # If there are reserved roles for the Agent and it is not already adopted
+            # then adopt it. It is important because of the Norms
+            agentRoles = observation.simDataServer.getReservedRolesForAgent(observation.agentData.id)
+            if any(agentRoles) and observation.agentMapcRole != agentRoles[0]:
+                return await self.planRoleAdoptPlan(observation, agentRoles[0])
 
-            # # If just initialized or reached target or target became known (and not relocating) then search for a new target
-            # if (self.currentTravelIntention is None or self.currentTravelIntention.checkFinished(observation)
-            #     or (not self.relocation and observation.map.getMapValueEnum(self.currentTravelIntention.coordinate) != MapValueEnum.UNKNOWN)):
+            # If just initialized or reached target or target became known (and not relocating) then search for a new target
+            if (self.currentTravelIntention is None or self.currentTravelIntention.checkFinished(observation)
+                or (not self.relocation and observation.map.getMapValueEnum(self.currentTravelIntention.coordinate) != MapValueEnum.UNKNOWN)):
 
-            #     # Try to search for a close unknown Coordinate
-            #     self.relocation = False
-            #     newDestination = observation.map.findClosestUnknownFromStartingLocation(
-            #             observation.agentStartingCoordinate, observation.agentCurrentCoordinate, observation.agentMapcRole.vision)
+                # Try to search for a close unknown Coordinate
+                self.relocation = False
+                newDestination = observation.map.findClosestUnknownFromStartingLocation(
+                        observation.agentStartingCoordinate, observation.agentCurrentCoordinate, observation.agentMapcRole.vision)
 
-            #     # If not found a close unknown Coordinate then move to a 
-            #     # random close Coordinate and try later there
-            #     if newDestination is None:
-            #         newDestination = observation.map.findRandomFarCoordinate(
-            #             observation.agentCurrentCoordinate, observation.agentMapcRole.vision)
-            #         self.relocation = True
+                # If not found a close unknown Coordinate then move to a 
+                # random close Coordinate and try later there
+                if newDestination is None:
+                    newDestination = observation.map.findRandomFarCoordinate(
+                        observation.agentCurrentCoordinate, observation.agentMapcRole.vision)
+                    self.relocation = True
                 
-            #     self.currentTravelIntention = TravelIntention(newDestination)
+                self.currentTravelIntention = TravelIntention(newDestination)
 
-            # return await self.currentTravelIntention.planNextAction(observation)
+            return await self.currentTravelIntention.planNextAction(observation)
 
             # return SkipAction()
             
@@ -94,9 +95,17 @@ class AttackIntention(AgentIntention):
                 coord not in observation.map.goalZones,
                 observation.agentCurrentCoordinate.neighbors(True, observation.agentMapcRole.clearMaxDistance)))
         
-        return min([coord for coord in clearableCoords],
-            key = lambda coord: Coordinate.manhattanDistance(coord,observation.agentCurrentCoordinate),
-            default = None)
+        if len(clearableCoords) > 1:
+            return min([coord for coord in clearableCoords],
+                key = lambda coord: Coordinate.manhattanDistance(coord,observation.agentCurrentCoordinate),
+                default = None)
+        elif len(clearableCoords) == 1:
+            if random.randint(1,10) <= 7:
+                return min([coord for coord in clearableCoords],
+                key = lambda coord: Coordinate.manhattanDistance(coord,observation.agentCurrentCoordinate),
+                default = None)
+        else:
+            return None
     
     def checkFinished(self, observation: Observation) -> bool:
         return observation.simDataServer.getMapCount() == 1 and observation.map.isMapExplored()
